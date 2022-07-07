1ab2539c00687d4789db10dc3bb1669f9c9abf99
MartinTheWaterdrinker <maksemiha@gmail.com>
2022-07-07 09:27:13 +0200

    finalized it

---
+++ src/example.js
@@ -0,0 +1,30 @@
+import "./App.css"
+import React, { Dropdown } from "semantic-ui-react"
+import { render } from "@testing-library/react"
+import { Component } from "react"
+const options = [
+  { key: 1, text: "1", value: 1 },
+  { key: 2, text: "2", value: 2 },
+  { key: 3, text: "3", value: 3 },
+]
+//function App(){
+export default class DropdownExampleUncontrolled extends Component {
+  state = {}
+  handleValue = (e, { value }) => this.setState({ value })
+  render() {
+    const { value } = this.state
+    return (
+      <div>
+        <Dropdown
+          onChange={this.handleValue}
+          options={options}
+          placeholder="choose an option"
+          selection
+          value={value}
+        />
+        <span> value={this.state.value} </span>
+      </div>
+    )
+  }
+}
+//export default App
